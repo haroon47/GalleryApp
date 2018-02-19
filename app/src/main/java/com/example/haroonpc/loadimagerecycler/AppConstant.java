8cdcfd44de67c6c997396783da3bdb8e802075d2
haroon47 <haroonfazal@outlook.com>
2018-02-19 21:51:43 +0500

    Initial commit

---
+++ app/src/main/java/com/example/haroonpc/loadimagerecycler/AppConstant.java
@@ -0,0 +1,14 @@
+package com.example.haroonpc.loadimagerecycler;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AppConstant {
+
+    // SD card image directory
+    public static final String PHOTO_ALBUM = "SoftScanner";
+
+    // supported file formats
+    public static final List<String> FILE_EXTN = Arrays.asList("jpg", "jpeg",
+            "png");
+}
